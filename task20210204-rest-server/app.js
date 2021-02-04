7c6eb904aa73dbee74b67d4c4b58d4f1df711863
baraboulka <75359803+baraboulka@users.noreply.github.com>
2021-02-04 19:22:56 +0200

    Simple REST functions created

---
+++ task20210204-rest-server/app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+const ObjectId = require("mongodb").ObjectID;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const database = "mongodb://localhost:27017/server1";
+
+const connectionEstablished = (err, client) => {
+  try {
+    db = client.db();
+    console.log("Connected to database!");
+    app.listen(3000, console.log("Listening on port 3000"));
+  } catch {
+    console.log(err);
+  }
+};
+
+mongodb.MongoClient.connect(
+  database,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  connectionEstablished
+);
+
+//DONE
+app.get("/", (req, res) => {
+  res.send("Welcome to server!");
+});
+
+//DONE
+app.get("/users", (req, res) => {
+  db.collection("users")
+    .find()
+    .toArray(function (e, items) {
+      if (e) {
+        res.status(400).json({ message: e });
+      } else {
+        res.send(items);
+      }
+    });
+});
+
+//DONE
+app.post("/users", (req, res) => {
+  try {
+    console.log(req.body);
+    const user = {
+      name: req.body.name,
+      age: req.body.age,
+    };
+    db.collection("users").insertOne(user);
+    res.send({ success: true, message: "User added successfully", user });
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+});
+
+//DONE
+app.get("/users/:id", (req, res) => {
+  db.collection("users").findOne(
+    {
+      _id: ObjectId(req.params.id),
+    },
+    (err, user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).json({ message: err });
+      }
+    }
+  );
+});
+
+//DONE
+app.put("/users/:id", (req, res) => {
+  db.collection("users").findOneAndUpdate(
+    { _id: ObjectId(req.params.id) },
+    { $set: { name: req.body.name, age: req.body.age } },
+    (err, user) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      } else {
+        res.send(`User ${user.value.name} updated!`);
+      }
+    }
+  );
+});
+
+app.delete("/users/:id", (req, res) => {
+  db.collection("users").findOneAndDelete(
+    { _id: ObjectId(req.params.id) },
+    (err, user) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      } else {
+        res.send(`User ${user.value.name} deleted!`);
+      }
+    }
+  );
+});
